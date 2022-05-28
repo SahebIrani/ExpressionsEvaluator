a525518a1f2f1194dd9acd3f296ab108e5959626
SinjulMSBH <sinjul.msbh@yahoo.com>
2022-05-28 23:16:58 +0200

    Add project files.

---
+++ ExpressionsEvaluator/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using System.Data;
+
+Console.WriteLine("Hello from Expressions Evaluator .. !!!!\n");
+
+var data = "1+2*3/6";
+
+Console.WriteLine($"Data: \"{data}\"\n");
+
+var result = new DataTable().Compute(data, default);
+
+Console.WriteLine($"Result: {result}");
+
+Console.ReadLine();
